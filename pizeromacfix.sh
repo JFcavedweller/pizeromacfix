fa491f6dcf598d414f7ce68b2a6d46050bf26fa7
JFcavedweller <61467313+JFcavedweller@users.noreply.github.com>
2020-06-12 15:51:30 -0400

    Update pizeromacfix.sh

--- pizeromacfix.sh
+++ pizeromacfix.sh
@@ -10,19 +10,21 @@ modifiedDate="2020-06-12"
 #---------------------------------------------------------------------------------------
 macprefix="00:e0:4c:FF:FF:" #standardized prefix for the new mac address. can be customized
 pizeronum=$(hostname | grep -Eo '[0-9]{2}$') #retrieves the two numeric digits at the end of the hostname. assumes hostname ends as such
-checking network interfaces...
-for interface in $(ifconfig | grep -Eio '^[[:alnum:]]*')
-do
-	if ["$(ifconfig $interface | grep ether)" == "00:e0:4c:53:44:58" ]
+echo "checking network interfaces..."
+interfaces=($(ifconfig | grep -Eio '^[[:alnum:]]*'))
+for i in ${interfaces[*]}; do
+	echo "interface $i"
+	macaddress="$(ifconfig $i | grep ether | grep -Eo '[[:alnum:]\:]{17}')"
+	echo "mac address is $macaddress"
+	if ["$macaddress" == "00:e0:4c:53:44:58" ]
 	then
-		echo "00:e0:4c:53:44:58 found on $interface. beginning spoof"
-		sudo ip link set $interface down #disables interface
+		echo "00:e0:4c:53:44:58 found on $i. beginning spoof"
+		sudo ip link set $i down #disables interface
 		timeout=30 #number of loop cycles (1s sleep) before aborting to prevent infinite loop
-		while true #loop to ensure that the interface is down before attempting to spoof
-		do
-			if $(ifconfig | grep $interface > /dev/nul) #not using grep will result in a false positive
+		while true; do #loop to ensure that the interface is down before attempting to spoof
+			if $(ifconfig | grep $i > /dev/nul) #not using grep will result in a false positive
 			then
-				echo "Waiting for $interface to be disabled..."
+				echo "Waiting for $i to be disabled..."
 				sleep 1
 				let timeout--
 				continue
@@ -31,17 +33,18 @@ do
 				"Timeout reached. Moving on."
 				break
 			else
-				"$interface is down. Moving on."
+				"$i is down. Moving on."
 				break
 			fi
 		done
 		sudo ip link set $interface address "$macprefix""$pizeronum"
-		sudo ip link set $interface up
-		if ["$(ifconfig $interface | grep ether)" == "00:e0:4c:53:44:58" ] #checks again to verify that the mac address has been updated successfully
+		sudo ip link set $i up
+		macaddress="$(ifconfig $i | grep ether | awk -F ' ' '{print $2}')"
+		if ["$macaddress" == "00:e0:4c:53:44:58" ] #checks again to verify that the mac address has been updated successfully
 		then
-			echo "MAC address update successful for $interface."
+			echo "MAC address update successful for $i."
 		else
-			echo "MAC address update was unsuccessful for $interface."
+			echo "MAC address update was unsuccessful for $i."
 		fi
 	fi
 done
